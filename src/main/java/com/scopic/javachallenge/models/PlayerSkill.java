b138840b5aaf7203ebbb4e1c3cbaa449af12331d
abishek-metadesign <123925501+abishek-metadesign@users.noreply.github.com>
2023-02-15 18:49:35 +0545

    Initial commit

---
+++ src/main/java/com/scopic/javachallenge/models/PlayerSkill.java
@@ -0,0 +1,71 @@
+// /////////////////////////////////////////////////////////////////////////////
+// PLEASE DO NOT RENAME OR REMOVE ANY OF THE CODE BELOW.
+// YOU CAN ADD YOUR CODE TO THIS FILE TO EXTEND THE FEATURES TO USE THEM IN YOUR WORK.
+// /////////////////////////////////////////////////////////////////////////////
+
+package com.scopic.javachallenge.models;
+
+import com.scopic.javachallenge.enums.Skill;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "player_skills")
+public class PlayerSkill implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Skill skill;
+
+    private Integer value;
+
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    private Player player;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
